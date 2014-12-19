a0494321cf689805581af1ffd3fb0c4916ae7ab0
kimmari10 <kimmari10@naver.com>
2014-12-19 17:10:11 +0900

    id, password 검증하는 코드 작성 및 공통 메소드 추출

--- src/main/java/security/AuthServiceTest.java
+++ src/main/java/security/AuthServiceTest.java
@@ -8,23 +8,30 @@ import org.junit.Test;
 
 public class AuthServiceTest {
 
+	private static final String USER_PASSWORD = "userPassword";
 	private AuthService authService;
 
 	public class AuthService {
 
 		public void authenticate(String id, String password) {
-			if(id == null) throw new IllegalArgumentException();
+			if(id == null || id.isEmpty()) throw new IllegalArgumentException();
+			if(password == null || password.isEmpty()) throw new IllegalArgumentException();
 		}
 
 	}
 	
 	@Test
 	public void givenInvalidId_throwIllegalArgEx() {
+		assertIllegalArgExThrown(null, USER_PASSWORD);
+		assertIllegalArgExThrown("", USER_PASSWORD);
+		assertIllegalArgExThrown("userId", null);
+		assertIllegalArgExThrown("userId", "");
+	}
+
+	private void assertIllegalArgExThrown(String id, String password) {
 		Exception throwEx = null;
-		
-		//alt + shift + z
 		try {
-			authService.authenticate(null, "userPassword");
+			authService.authenticate(id, password);
 		} catch (Exception e) {
 			throwEx = e;
 		}
@@ -33,7 +40,6 @@ public class AuthServiceTest {
 	
 	@Before
 	public void setUp() {
-		//변수명 alt + shift + t -> Convert local variable to field
 		authService = new AuthService();
 		
 	}
