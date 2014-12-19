949af07b2ed5328981e1a6e30019883494e1e0a7
kimmari10 <kimmari10@naver.com>
2014-12-19 17:33:06 +0900

    WrongPasswordException 테스트 21:20

--- src/main/java/security/AuthServiceTest.java
+++ src/main/java/security/AuthServiceTest.java
@@ -1,6 +1,6 @@
 package security;
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +8,17 @@ import org.junit.Test;
 
 public class AuthServiceTest {
 
+	public class WrongPasswordException extends RuntimeException {
+
+	}
+
+	private static final String NO_USER_ID = "noUserId";
+
+	public class NonExistingUserException extends RuntimeException {
+
+	}
+
+	private static final String USER_ID = "userId";
 	private static final String USER_PASSWORD = "userPassword";
 	private AuthService authService;
 
@@ -16,26 +27,68 @@ public class AuthServiceTest {
 		public void authenticate(String id, String password) {
 			if(id == null || id.isEmpty()) throw new IllegalArgumentException();
 			if(password == null || password.isEmpty()) throw new IllegalArgumentException();
+			
+			User user = findUserById(id);
+			if(user == null)
+				throw new NonExistingUserException();
+			
+			throw new WrongPasswordException();
+		}
+
+		private User findUserById(String id) {
+			if (id.equals("userId"))
+				return new User(id, "1234");
+			return null;
 		}
 
 	}
 	
 	@Test
-	public void givenInvalidId_throwIllegalArgEx() {
-		assertIllegalArgExThrown(null, USER_PASSWORD);
-		assertIllegalArgExThrown("", USER_PASSWORD);
-		assertIllegalArgExThrown("userId", null);
-		assertIllegalArgExThrown("userId", "");
+	public void whenUserFoundButWrongPw_throwWrongPasswordEx() {
+		givenUserExists("userId", USER_PASSWORD);
+		assertExceptionThrown("userId", "worngPassword", WrongPasswordException.class);
+		verifyUserFound("userId");
+		
+	}	
+	
+	private void givenUserExists(String string, String userPassword) {
+		// TODO Auto-generated method stub
+		
 	}
 
-	private void assertIllegalArgExThrown(String id, String password) {
+	private void verifyUserFound(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Test
+	public void whenUserNotFound_throwNonExistingUserEx() {
+		assertExceptionThrown(NO_USER_ID, USER_PASSWORD, NonExistingUserException.class);
+		for (int i = 1; i <= 100; i++)
+			assertExceptionThrown(NO_USER_ID + i, USER_PASSWORD, NonExistingUserException.class);
+	}
+
+	private void assertExceptionThrown(String id, String password,
+			Class<? extends Exception> type) {
 		Exception throwEx = null;
 		try {
 			authService.authenticate(id, password);
 		} catch (Exception e) {
 			throwEx = e;
 		}
-		assertThat(throwEx, instanceOf(IllegalArgumentException.class));
+		assertThat(throwEx, instanceOf(type));
+	}
+	
+	@Test
+	public void givenInvalidId_throwIllegalArgEx() {
+		assertIllegalArgExThrown(null, USER_PASSWORD);
+		assertIllegalArgExThrown("", USER_PASSWORD);
+		assertIllegalArgExThrown(USER_ID, null);
+		assertIllegalArgExThrown(USER_ID, "");
+	}
+
+	private void assertIllegalArgExThrown(String id, String password) {
+		assertExceptionThrown(id, password, IllegalArgumentException.class);
 	}
 	
 	@Before
---
+++ src/main/java/security/User.java
@@ -0,0 +1,9 @@
+package security;
+
+public class User {
+
+	public User(String id, String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
