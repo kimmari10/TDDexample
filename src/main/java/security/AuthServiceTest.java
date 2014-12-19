eb59fe04054a35e0d649b7255fb60ed37ad12092
kimmari10 <kimmari10@naver.com>
2014-12-19 16:50:29 +0900

    @Before 로 변경, 공통부분 필드 변수로 변경

--- src/main/java/security/AuthServiceTest.java
+++ src/main/java/security/AuthServiceTest.java
@@ -1,14 +1,40 @@
 package security;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.springframework.context.annotation.Bean;
 
 
 public class AuthServiceTest {
 
+	private AuthService authService;
+
+	public class AuthService {
+
+		public void authenticate(String id, String password) {
+			if(id == null) throw new IllegalArgumentException();
+		}
+
+	}
+	
 	@Test
-	public void nothing() {
+	public void givenInvalidId_throwIllegalArgEx() {
+		Exception throwEx = null;
+		
+		//alt + shift + z
+		try {
+			authService.authenticate(null, "userPassword");
+		} catch (Exception e) {
+			throwEx = e;
+		}
+		assertThat(throwEx, instanceOf(IllegalArgumentException.class));
+	}
+	
+	@Before
+	public void setUp() {
+		//변수명 alt + shift + t -> Convert local variable to field
+		authService = new AuthService();
 		
 	}
 	
