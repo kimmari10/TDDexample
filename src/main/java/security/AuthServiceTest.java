8930e0bd75f6ddfe193b2fcaadefe6f179c8adb0
kimmari10 <kimmari10@naver.com>
2014-12-19 16:29:40 +0900

    nothing 테스트 메소드 작성

---
+++ src/main/java/security/AuthServiceTest.java
@@ -0,0 +1,16 @@
+package security;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.context.annotation.Bean;
+
+
+public class AuthServiceTest {
+
+	@Test
+	public void nothing() {
+		
+	}
+	
+	
+}
